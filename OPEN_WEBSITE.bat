38606a84b375d46a3c7b19faf2d18bd7d9a474b3
Ashley Edds <ashley@edds.net>
2026-01-26 17:14:01 -0500

    remove MD files and bat scripts not needed, add new logo

--- OPEN_WEBSITE.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Opening Raleigh Home Automation Website...
-cd /d "%~dp0"
-start index.html
-echo Website opened in your default browser!
-pause
--- START_SERVER.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Starting Raleigh Home Automation Website Server...
-echo.
-cd /d "%~dp0"
-powershell -ExecutionPolicy Bypass -File server.ps1
-pause
